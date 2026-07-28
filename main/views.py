729d06b600eeeaf0cdbac62fc0ecb0c4388f85a5
infinite-dynamics <orlova.nastya04@mail.ru>
2026-07-29 01:24:59 +0300

    Registered simple tag of categories

--- main/views.py
+++ main/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
 
     context = {
         'title': 'Тепло в деталях - Главная',
         'content': 'Магазин ручных товаров "Тепло в деталях"',
-        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
