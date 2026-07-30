f6fb3720490bf776ba29d65f6c8ed327444d3afc
infinite-dynamics <orlova.nastya04@mail.ru>
2026-07-31 01:45:40 +0300

    PostgreSQL data base connected & full text search

--- thesis/settings.py
+++ thesis/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'debug_toolbar',
     
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'thesis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
